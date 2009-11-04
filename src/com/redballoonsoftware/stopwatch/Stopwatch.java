195c6aa8877be14315e23d98dd2ccfba732309fe
Daniel Bostwick <daniel@Daniels-MacBook-Pro.local>
2009-11-03 22:42:21 -0800

    Fix NullPointerException bug in Stopwatch Activity

--- src/com/redballoonsoftware/stopwatch/Stopwatch.java
+++ src/com/redballoonsoftware/stopwatch/Stopwatch.java
@@ -18,15 +18,15 @@ public class Stopwatch extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        Log.d(TAG, "Setting View");
+        
+        setContentView(R.layout.stopwatch);
+        
         Log.d(TAG, "Setting members");
                 
         mStopwatch = (com.redballoonsoftware.widgets.Stopwatch)findViewById(R.id.Stopwatch01);
         mStartStop = (Button)findViewById(R.id.StartStopButton);
         mResetLap = (Button)findViewById(R.id.ResetLapButton);
-        
-        Log.d(TAG, "Setting View");
-        
-        setContentView(R.layout.stopwatch);
     }
     
     
@@ -39,42 +39,4 @@ public class Stopwatch extends Activity {
     	Log.d(TAG, "onResetLapButtonClick called");
     }
     
-    /*
-    private void setOnClickListeners() {
-    	
-        findViewById(R.id.StartStopButton).setOnClickListener(
-    		new View.OnClickListener() {
-    			@Override
-    			public void onClick(View v) {
-    		    	Log.d(TAG, "onStartStopButtonClick called");
-    		    	if (mStopwatch.isStarted() && !mStopwatch.isPaused()) {
-    		    		Log.d(TAG, "stopping");
-    		    		mStopwatch.stop();
-    		    		mStartStop.setText("Start");
-    		    		mResetLap.setText("Reset");
-    		    	} else {
-    		    		Log.d(TAG, "starting");
-    		    		mStopwatch.start();
-    		    		mStartStop.setText("Stop");
-    		    		mResetLap.setTag("Lap");
-    		    	}
-    			}
-    		}
-        );
-
-        findViewById(R.id.ResetLapButton).setOnClickListener(
-        		new View.OnClickListener() {
-        			@Override
-        			public void onClick(View v) {
-        		    	if (mStopwatch.isStarted() && !mStopwatch.isPaused()) {
-        		    		// Lap
-        		    	} else {
-        		    		mStopwatch.reset();
-        		    	}
-        			}
-        		}
-        );
-		
-    }
-	*/
 }
--- src/com/redballoonsoftware/widgets/Stopwatch.java
+++ src/com/redballoonsoftware/widgets/Stopwatch.java
@@ -1,163 +1,271 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.redballoonsoftware.widgets;
+
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
+import android.text.format.DateUtils;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.View;
 import android.widget.TextView;
-import java.util.Timer;
-import java.util.TimerTask;
+import android.widget.RemoteViews.RemoteView;
 
+import java.util.Formatter;
+import java.util.IllegalFormatException;
+import java.util.Locale;
+
+/**
+ * Class that implements a simple timer.
+ * <p>
+ * You can give it a start time in the {@link SystemClock#elapsedRealtime} timebase,
+ * and it counts up from that, or if you don't give it a base time, it will use the
+ * time at which you call {@link #start}.  By default it will display the current
+ * timer value in the form "MM:SS" or "H:MM:SS", or you can use {@link #setFormat}
+ * to format the timer value into an arbitrary string.
+ *
+ * @attr ref android.R.styleable#Chronometer_format
+ */
+@RemoteView
 public class Stopwatch extends TextView {
-	private static final String TAG = "Stopwatch";
-	private static final int TICK_WHAT = 2;
-	
-	// in milliseconds
-	private long mStartTime;
-	private long mNow;
-	private long mNowOffset;
-	private long mDelay;
-	
-	private boolean mPaused;
-	private boolean mRunning;
-	
-	private Handler mHandler = new Handler();
-	
-	
-	private Runnable mUpdateTimeTask = new Runnable() {
-		@Override
-		public void run() {
-			mNow = (SystemClock.elapsedRealtime() - mStartTime) + mNowOffset;
-			updateText(mNow);
-			mHandler.postDelayed(this, mDelay);
-		}
-	};
-	
-	public void start() {
-		if ( !mRunning ) {
-			mStartTime = SystemClock.elapsedRealtime();
-			mRunning = true;
-			mHandler.removeCallbacks(mUpdateTimeTask);
-			mHandler.postDelayed(mUpdateTimeTask, mDelay);
-		}
-	}
-	
-	
-	
-	public void stop() {
-		mRunning = false;
-		mHandler.removeCallbacks(mUpdateTimeTask);
-	}
-	
-	public void reset() {
-		mNow = 0;
-		mRunning = false;
-	}
+    private static final String TAG = "Chronometer";
 
-	
-	public Stopwatch(Context context) {
-		this(context, null, 0);
-	}
-	
-	public Stopwatch(Context context, AttributeSet attrs) {
-		this(context, attrs, 0);
-	}
-	
-	public Stopwatch(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		init();
-	}
+    /**
+     * A callback that notifies when the chronometer has incremented on its own.
+     */
+    public interface OnStopwatchTickListener {
 
-	private void init() {
-		mDelay = 10; 	// Update every hundredth of a second
-		reset();
-		updateText(mNow);
-	}
-	
-	/* 
-	public void reset() {
-		// mTimer = new Timer();
-		mNow = 0;
-		mNowOffset = 0;
-		mIsPaused = true;
-		mIsStarted = false;
-	}
-	
-	public void start() {
-		Log.d(TAG, "start");
-		mIsPaused = false;
-		mIsStarted = true;
-		mStart = SystemClock.elapsedRealtime();
-		mHandler.removeCallbacks(mUpdateTimeTask);
-		mHandler.postDelayed(mUpdateTimeTask, mDelay);
-	}
-	
-	public void stop() {
-		Log.d(TAG, "stop");
-		mPaused = true;
-		mNowOffset = mNow;
-	}
-	*/
-	
-	public boolean isStarted() {
-		return mRunning;
-	}
-	
-	public boolean isPaused() {
-		return mPaused;
-	}
-	
-	private synchronized void updateText(long now) {
-		Log.d(TAG, "updateText");
-		setText(formatElapsedTime(now));
-	}
-	
-	/***
-	 * Given the time elapsed in tenths of seconds, returns the string
-	 * representation of that time. 
-	 * 
-	 * @param now, the current time in tenths of seconds
-	 * @return 	String with the current time in the format MM:SS.T or 
-	 * 			HH:MM:SS.T, depending on elapsed time.
-	 */
-	private String formatElapsedTime(long now) {
-		long hours=0, minutes=0, seconds=0, tenths=0;
-		StringBuilder sb = new StringBuilder();
-		
-		if (now < 1000) {
-			tenths = now / 100;
-		} else if (now < 60000) {
-			seconds = now / 1000;
-			now -= seconds * 1000;
-			tenths = (now / 100);
-		} else if (now < 3600000) {
-			hours = now / 3600000;
-			now -= hours * 3600000;
-			minutes = now / 60000;
-			now -= minutes * 60000;
-			seconds = now / 1000;
-			now -= seconds * 1000;
-			tenths = (now / 100);
-		}
-		
-		if (hours > 0) {
-			sb.append(hours).append(":")
-				.append(formatDigits(minutes)).append(":")
-				.append(formatDigits(seconds)).append(".")
-				.append(tenths);
-		} else {
-			sb.append(formatDigits(minutes)).append(":")
-			.append(formatDigits(seconds)).append(".")
-			.append(tenths);
-		}
-		
-		return sb.toString();
-	}
-	
-	private String formatDigits(long num) {
-		return (num < 10) ? "0" + num : new Long(num).toString();
-	}
+        /**
+         * Notification that the chronometer has changed.
+         */
+        void onStopwatchTick(Stopwatch chronometer);
 
+    }
+
+    private long mBase;
+    private boolean mVisible;
+    private boolean mStarted;
+    private boolean mRunning;
+    private boolean mLogged;
+    private String mFormat;
+    private Formatter mFormatter;
+    private Locale mFormatterLocale;
+    private Object[] mFormatterArgs = new Object[1];
+    private StringBuilder mFormatBuilder;
+    private OnStopwatchTickListener mOnStopwatchTickListener;
+    private StringBuilder mRecycle = new StringBuilder(8);
+    
+    private static final int TICK_WHAT = 2;
+    
+    /**
+     * Initialize this Stopwatch object.
+     * Sets the base to the current time.
+     */
+    public Stopwatch(Context context) {
+        this(context, null, 0);
+    }
+
+    /**
+     * Initialize with standard view layout information.
+     * Sets the base to the current time.
+     */
+    public Stopwatch(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    /**
+     * Initialize with standard view layout information and style.
+     * Sets the base to the current time.
+     */
+    public Stopwatch(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+        init();
+    }
+
+    private void init() {
+        mBase = SystemClock.elapsedRealtime();
+        updateText(mBase);
+    }
+
+    /**
+     * Set the time that the count-up timer is in reference to.
+     *
+     * @param base Use the {@link SystemClock#elapsedRealtime} time base.
+     */
+    public void setBase(long base) {
+        mBase = base;
+        dispatchStopwatchTick();
+        updateText(SystemClock.elapsedRealtime());
+    }
+
+    /**
+     * Return the base time as set through {@link #setBase}.
+     */
+    public long getBase() {
+        return mBase;
+    }
+
+    /**
+     * Sets the format string used for display.  The Stopwatch will display
+     * this string, with the first "%s" replaced by the current timer value in
+     * "MM:SS" or "H:MM:SS" form.
+     *
+     * If the format string is null, or if you never call setFormat(), the
+     * Stopwatch will simply display the timer value in "MM:SS" or "H:MM:SS"
+     * form.
+     *
+     * @param format the format string.
+     */
+    public void setFormat(String format) {
+        mFormat = format;
+        if (format != null && mFormatBuilder == null) {
+            mFormatBuilder = new StringBuilder(format.length() * 2);
+        }
+    }
+
+    /**
+     * Returns the current format string as set through {@link #setFormat}.
+     */
+    public String getFormat() {
+        return mFormat;
+    }
+
+    /**
+     * Sets the listener to be called when the chronometer changes.
+     * 
+     * @param listener The listener.
+     */
+    public void setOnStopwatchTickListener(OnStopwatchTickListener listener) {
+        mOnStopwatchTickListener = listener;
+    }
+
+    /**
+     * @return The listener (may be null) that is listening for chronometer change
+     *         events.
+     */
+    public OnStopwatchTickListener getOnStopwatchTickListener() {
+        return mOnStopwatchTickListener;
+    }
+
+    /**
+     * Start counting up.  This does not affect the base as set from {@link #setBase}, just
+     * the view display.
+     * 
+     * Stopwatch works by regularly scheduling messages to the handler, even when the 
+     * Widget is not visible.  To make sure resource leaks do not occur, the user should 
+     * make sure that each start() call has a reciprocal call to {@link #stop}. 
+     */
+    public void start() {
+        mStarted = true;
+        updateRunning();
+    }
+
+    /**
+     * Stop counting up.  This does not affect the base as set from {@link #setBase}, just
+     * the view display.
+     * 
+     * This stops the messages to the handler, effectively releasing resources that would
+     * be held as the chronometer is running, via {@link #start}. 
+     */
+    public void stop() {
+        mStarted = false;
+        updateRunning();
+    }
+
+    /**
+     * The same as calling {@link #start} or {@link #stop}.
+     * @hide pending API council approval
+     */
+    public void setStarted(boolean started) {
+        mStarted = started;
+        updateRunning();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mVisible = false;
+        updateRunning();
+    }
+
+    @Override
+    protected void onWindowVisibilityChanged(int visibility) {
+        super.onWindowVisibilityChanged(visibility);
+        mVisible = visibility == VISIBLE;
+        updateRunning();
+    }
+
+    private synchronized void updateText(long now) {
+        long seconds = now - mBase;
+        seconds /= 1000;
+        String text = DateUtils.formatElapsedTime(mRecycle, seconds);
+
+        if (mFormat != null) {
+            Locale loc = Locale.getDefault();
+            if (mFormatter == null || !loc.equals(mFormatterLocale)) {
+                mFormatterLocale = loc;
+                mFormatter = new Formatter(mFormatBuilder, loc);
+            }
+            mFormatBuilder.setLength(0);
+            mFormatterArgs[0] = text;
+            try {
+                mFormatter.format(mFormat, mFormatterArgs);
+                text = mFormatBuilder.toString();
+            } catch (IllegalFormatException ex) {
+                if (!mLogged) {
+                    Log.w(TAG, "Illegal format string: " + mFormat);
+                    mLogged = true;
+                }
+            }
+        }
+        setText(text);
+    }
+
+    private void updateRunning() {
+        boolean running = mVisible && mStarted;
+        if (running != mRunning) {
+            if (running) {
+                updateText(SystemClock.elapsedRealtime());
+                dispatchStopwatchTick();
+                mHandler.sendMessageDelayed(Message.obtain(mHandler, TICK_WHAT), 1000);
+            } else {
+                mHandler.removeMessages(TICK_WHAT);
+            }
+            mRunning = running;
+        }
+    }
+    
+    private Handler mHandler = new Handler() {
+        public void handleMessage(Message m) {
+            if (mRunning) {
+                updateText(SystemClock.elapsedRealtime());
+                dispatchStopwatchTick();
+                sendMessageDelayed(Message.obtain(this, TICK_WHAT), 1000);
+            }
+        }
+    };
+
+    void dispatchStopwatchTick() {
+        if (mOnStopwatchTickListener != null) {
+            mOnStopwatchTickListener.onStopwatchTick(this);
+        }
+    }
 }
