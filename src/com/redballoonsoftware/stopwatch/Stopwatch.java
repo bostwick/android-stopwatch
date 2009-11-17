62b55ff4e2477d7e424ca4ef83cca19dbed2396d
Daniel Bostwick <daniel@Daniels-MacBook-Pro.local>
2009-11-16 21:41:36 -0800

    Lap list adds laps, clears

--- src/com/redballoonsoftware/stopwatch/Stopwatch.java
+++ src/com/redballoonsoftware/stopwatch/Stopwatch.java
@@ -1,17 +1,21 @@
 package com.redballoonsoftware.stopwatch;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class Stopwatch extends Activity {
+public class Stopwatch extends ListActivity {
 	private static String TAG = "StopwatchActivity";
 	private com.redballoonsoftware.widgets.Stopwatch mStopwatch;
 	
 	private Button mStartStop;
-	private Button mResetLap; 
+	private Button mResetLap;
+	private ListView mLapList; 
 	
     /** Called when the activity is first created. */
     @Override
@@ -27,6 +31,9 @@ public class Stopwatch extends Activity {
         mStopwatch = (com.redballoonsoftware.widgets.Stopwatch)findViewById(R.id.Stopwatch01);
         mStartStop = (Button)findViewById(R.id.StartStopButton);
         mResetLap = (Button)findViewById(R.id.ResetLapButton);
+        mLapList = getListView();
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.laps_row));
     }
     
     
@@ -47,8 +54,10 @@ public class Stopwatch extends Activity {
     	Log.d(TAG, "onResetLapButtonClick called");
     	if (mStopwatch.isPaused()) {    // currently says "Reset"
     	    mStopwatch.reset();
+    	    ((ArrayAdapter<String>)getListAdapter()).clear();
     	} else {
             Log.d(TAG, mStopwatch.currentFormattedTime());
+            ((ArrayAdapter<String>)mLapList.getAdapter()).insert(mStopwatch.currentFormattedTime(), 0);
     	}
     }
     
